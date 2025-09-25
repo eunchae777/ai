e7a280601460f5c8abd0eb6e62391ca0a43af19f
eunchae777 <eunchae777@gmail.com>
2025-09-25 17:59:35 +0900

    python 모듈과 패키지

---
+++ source/pylib/sample_pac/__init__.py
@@ -0,0 +1,9 @@
+'''
+sample_pac/ __inint__.py
+sample_pac 패키지를 import 할때 자동실행되는 파일입니다.
+'''
+print("samplie_pac 패키지를 로드했어요")
+
+def test():
+    print("sample_pac안의 test()입니다")
+    
---
+++ source/pylib/sample_pac/ab/b.py
@@ -0,0 +1,4 @@
+def world():
+    print('sample_pac.ab.b모듈의 world')
+if __name__ == '__main__':
+    world()
