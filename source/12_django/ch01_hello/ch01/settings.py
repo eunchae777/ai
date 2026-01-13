8d512d13d5d0c189deb4d899c814e45d6cd8766d
eunchae777 <eunchae777@gmail.com>
2026-01-13 17:46:23 +0900

    20260113

--- source/12_django/ch01_hello/ch01/settings.py
+++ source/12_django/ch01_hello/ch01/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4s-s@8od8q0!=8kj$5hc!^yoz1y$&xd3kr9z27xvfp72$^+fpp"
+from decouple import config
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "home",
 ]
 
 MIDDLEWARE = [
